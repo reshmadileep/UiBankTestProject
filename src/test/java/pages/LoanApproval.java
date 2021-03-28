5e1dd3a8c027a5fc419c8f9d7f997579f3d18d5d
Reshma <reshmadileep@yahoo.co.in>
2021-03-29 09:29:15 +1100

    Adding a print message.

--- src/test/java/pages/LoanApproval.java
+++ src/test/java/pages/LoanApproval.java
@@ -7,7 +7,10 @@ public class LoanApproval extends SetUp {
 	By txtAPR = By.xpath("//*[@id=\"rateValue\"]");
 	
 	
-	public String getAprValue() {		
+	public String getAprValue() {	
+		
+	if ((SetUp.driver.findElement(txtAPR)).isDisplayed())
+		System.out.println("Loan Application is successful");
 	
 	return	SetUp.driver.findElement(txtAPR).getText();
 	
